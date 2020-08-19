87a33222e8ed13fde0238b21ef89cf448fd82e1f
Yashika Bihani <yashikabihani7@gmail.com>
2020-08-19 19:05:31 +0530

    Basic Structure of the Whatsapp App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYI7FrwhydcEZxaLGVOIlDjGexgp7LiB0",
+    authDomain: "whatsapp-clone-7a7cb.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-7a7cb.firebaseio.com",
+    projectId: "whatsapp-clone-7a7cb",
+    storageBucket: "whatsapp-clone-7a7cb.appspot.com",
+    messagingSenderId: "208592985866",
+    appId: "1:208592985866:web:0e1b32705f7bbe5a38e4f3",
+    measurementId: "G-TFZMGPJQHY"
+  };
